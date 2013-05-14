a42f0f73ecef6b59bfa6a6b62177222afd5c9f4d
smvalkov <smvalkov@gmail.com>
2013-05-14 19:36:25 +0300

    Created a class GameBoard, that cbuilds the game board and adds the bombs.

--- BattleFieldGame.cs
+++ BattleFieldGame.cs
@@ -16,57 +16,11 @@ namespace BattleField
             int rows = gameFieldSize + 2;
             int cols = gameFieldSize * 2 + 2;
 
-            String[,] gameField = new String[rows, cols];
 
-            gameField[0, 0] = " ";
-            gameField[0, 1] = " ";
-            gameField[1, 0] = " ";
-            gameField[1, 1] = " ";
-
-            for (int row = 2; row < rows; row++)
-            {
-                for (int col = 2; col < cols; col++)
-                {
-                    if (col % 2 == 0)
-                    {
-                        if (col == 2)
-                        {
-                            gameField[0, col] = "0";
-                        }
-                        else
-                        {
-                            gameField[0, col] = Convert.ToString((col - 2) / 2);
-                        }
-                    }
-                    else
-                    {
-                        gameField[0, col] = " ";
-                    }
-                    if (col < cols - 1)
-                    {
-                        gameField[1, col] = "-";
-                    }
-
-                    gameField[row, 0] = Convert.ToString(row - 2);
-                    gameField[row, 1] = "|";
-                    if (col % 2 == 0)
-                    {
-                        gameField[row, col] = "-";
-                    }
-                    else
-                    {
-                        gameField[row, col] = " ";
-                    }
-
-                }
-
-            }
-
-            Methods.AddBombs(gameField);
-            Methods.PrintArray(gameField);
+            Board gameField = new Board(rows, cols);            
+            Methods.PrintArray(gameField.GameBoard);
             int countPlayed = 0;
-            Methods.PlayerTurn(gameField, countPlayed);
-
+            Methods.PlayerTurn(gameField.GameBoard, countPlayed);
         }
 
         private static void ReadInput()
@@ -75,10 +29,8 @@ namespace BattleField
             gameFieldSize = Convert.ToInt32(Console.ReadLine());
             while (gameFieldSize < 1 || gameFieldSize > 10)
             {
-
                 Console.WriteLine("Enter a number between 1 and 10!");
                 gameFieldSize = Convert.ToInt32(Console.ReadLine());
-
             }
         }
     }
---
+++ GameBoard.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BattleField
+{
+    class Board
+    {
+        private int rows = 0;
+        private int cols = 0;
+        private string[,] gameBoard = new string[1, 1];
+
+        public int Rows 
+        {
+            get
+            {
+                return this.rows;
+            }
+            protected set
+            {
+                this.rows = value;
+            }        
+        }
+
+        public int Cols 
+        {
+            get
+            {
+                return this.cols;
+            }
+            protected set
+            {
+                this.cols = value;
+            } 
+        }
+
+        public string[,] GameBoard 
+        {
+            get 
+            {
+                return this.gameBoard;
+            }
+            protected set 
+            {
+                this.gameBoard = value;
+            } 
+        }
+
+        public Board(int rows, int cols) 
+        {
+            this.Rows = rows;
+            this.Cols = cols;
+            this.GameBoard = CreateGameBoard();
+        }
+
+        private string[,] CreateGameBoard()
+        {
+            string[,] gameBoard = new string[rows, cols];
+
+            gameBoard[0, 0] = " ";
+            gameBoard[0, 1] = " ";
+            gameBoard[1, 0] = " ";
+            gameBoard[1, 1] = " ";
+
+            for (int row = 2; row < rows; row++)
+            {
+                for (int col = 2; col < cols; col++)
+                {
+                    if (col % 2 == 0)
+                    {
+                        if (col == 2)
+                        {
+                            gameBoard[0, col] = "0";
+                        }
+                        else
+                        {
+                            gameBoard[0, col] = Convert.ToString((col - 2) / 2);
+                        }
+                    }
+                    else
+                    {
+                        gameBoard[0, col] = " ";
+                    }
+                    if (col < cols - 1)
+                    {
+                        gameBoard[1, col] = "-";
+                    }
+
+                    gameBoard[row, 0] = Convert.ToString(row - 2);
+                    gameBoard[row, 1] = "|";
+                    if (col % 2 == 0)
+                    {
+                        gameBoard[row, col] = "-";
+                    }
+                    else
+                    {
+                        gameBoard[row, col] = " ";
+                    }
+                }
+            }
+            
+            Methods.AddBombs(gameBoard);
+            
+            return gameBoard;
+        }        
+    }
+}
