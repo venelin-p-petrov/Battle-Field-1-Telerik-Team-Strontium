c0980e66de304200133db10939969424f4caa0f6
Ivo Krastev <ivo.k.krastev@gmail.com>
2013-05-17 11:53:56 +0200

    ExplosionHandler UnitTests Finished!

--- BattleField.Test/ExplosionHandlerTest.cs
+++ BattleField.Test/ExplosionHandlerTest.cs
@@ -10,41 +10,441 @@ namespace BattleField.Test
         [TestMethod]
         public void HitMineTest_sizeOne()
         {
-            string[,] initialBoard = new string[3, 3]
+            const int testMatrixSize = 3;
+
+            string[,] initialBoard1 = new string[testMatrixSize, testMatrixSize]
             {
                 {"-", "-", "-"},
                 {"-", "1", "-"},
                 {"-", "-", "-"}
             };
-
-            string[,] expectedBoard = new string[3, 3]
+            string[,] expectedBoard1 = new string[testMatrixSize, testMatrixSize]
             {
                 {"X", "-", "X"},
                 {"-", "X", "-"},
                 {"X", "-", "X"}
             };
+            Assert.IsTrue(CheckForEqualOutput(testMatrixSize, 1, 1, initialBoard1, expectedBoard1));
 
-            Assert.IsTrue(CheckIfEqual(3, initialBoard, expectedBoard));
+            string[,] initialBoard2 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"-", "-", "1"},
+                {"-", "-", "-"},
+                {"-", "-", "-"}
+            };
+            string[,] expectedBoard2 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"-", "-", "X"},
+                {"-", "X", "-"},
+                {"-", "-", "-"}
+            };
+            Assert.IsTrue(CheckForEqualOutput(testMatrixSize, 0, 2, initialBoard2, expectedBoard2));
+
+            string[,] initialBoard3 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"1", "-", "-"},
+                {"-", "-", "-"},
+                {"-", "-", "-"}
+            };
+            string[,] expectedBoard3 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"X", "-", "-"},
+                {"-", "X", "-"},
+                {"-", "-", "-"}
+            };
+            Assert.IsTrue(CheckForEqualOutput(testMatrixSize, 0, 0, initialBoard3, expectedBoard3));
+
+            string[,] initialBoard4 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"-", "-", "-"},
+                {"-", "-", "-"},
+                {"1", "-", "-"}
+            };
+            string[,] expectedBoard4 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"-", "-", "-"},
+                {"-", "X", "-"},
+                {"X", "-", "-"}
+            };
+            Assert.IsTrue(CheckForEqualOutput(testMatrixSize, 2, 0, initialBoard4, expectedBoard4));
+
+            string[,] initialBoard5 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"-", "-", "-"},
+                {"-", "-", "-"},
+                {"-", "-", "1"}
+            };
+            string[,] expectedBoard5 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"-", "-", "-"},
+                {"-", "X", "-"},
+                {"-", "-", "X"}
+            };
+            Assert.IsTrue(CheckForEqualOutput(testMatrixSize, 2, 2, initialBoard5, expectedBoard5));
         }
 
         [TestMethod]
         public void HitMineTest_sizeTwo()
         {
-            string[,] initialBoard = new string[3, 3]
+            const int testMatrixSize = 3;
+
+            string[,] initialBoard1 = new string[testMatrixSize, testMatrixSize]
             {
                 {"-", "-", "-"},
                 {"-", "2", "-"},
                 {"-", "-", "-"}
             };
-
-            string[,] expectedBoard = new string[3, 3]
+            string[,] expectedBoard1 = new string[testMatrixSize, testMatrixSize]
             {
                 {"X", "X", "X"},
                 {"X", "X", "X"},
                 {"X", "X", "X"}
             };
+            Assert.IsTrue(CheckForEqualOutput(testMatrixSize, 1, 1, initialBoard1, expectedBoard1));
 
-            Assert.IsTrue(CheckIfEqual(3, initialBoard, expectedBoard));
+            string[,] initialBoard2 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"-", "-", "2"},
+                {"-", "-", "-"},
+                {"-", "-", "-"}
+            };
+            string[,] expectedBoard2 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"-", "X", "X"},
+                {"-", "X", "X"},
+                {"-", "-", "-"}
+            };
+            Assert.IsTrue(CheckForEqualOutput(testMatrixSize, 0, 2, initialBoard2, expectedBoard2));
+
+            string[,] initialBoard3 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"2", "-", "-"},
+                {"-", "-", "-"},
+                {"-", "-", "-"}
+            };
+            string[,] expectedBoard3 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"X", "X", "-"},
+                {"X", "X", "-"},
+                {"-", "-", "-"}
+            };
+            Assert.IsTrue(CheckForEqualOutput(testMatrixSize, 0, 0, initialBoard3, expectedBoard3));
+
+            string[,] initialBoard4 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"-", "-", "-"},
+                {"-", "-", "-"},
+                {"2", "-", "-"}
+            };
+            string[,] expectedBoard4 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"-", "-", "-"},
+                {"X", "X", "-"},
+                {"X", "X", "-"}
+            };
+            Assert.IsTrue(CheckForEqualOutput(testMatrixSize, 2, 0, initialBoard4, expectedBoard4));
+
+            string[,] initialBoard5 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"-", "-", "-"},
+                {"-", "-", "-"},
+                {"-", "-", "2"}
+            };
+            string[,] expectedBoard5 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"-", "-", "-"},
+                {"-", "X", "X"},
+                {"-", "X", "X"}
+            };
+            Assert.IsTrue(CheckForEqualOutput(testMatrixSize, 2, 2, initialBoard5, expectedBoard5));
+        }
+
+        [TestMethod]
+        public void HitMineTest_sizeThree()
+        {
+            const int testMatrixSize = 5;
+
+            string[,] initialBoard1 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "3", "-", "-"},
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"}
+            };
+            string[,] expectedBoard1 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"-", "-", "X", "-", "-"},
+                {"-", "X", "X", "X", "-"},
+                {"X", "X", "X", "X", "X"},
+                {"-", "X", "X", "X", "-"},
+                {"-", "-", "X", "-", "-"}
+            };
+            Assert.IsTrue(CheckForEqualOutput(testMatrixSize, 2, 2, initialBoard1, expectedBoard1));
+
+            string[,] initialBoard2 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"3", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"}
+            };
+            string[,] expectedBoard2 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"X", "X", "X", "-", "-"},
+                {"X", "X", "-", "-", "-"},
+                {"X", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"}
+            };
+            Assert.IsTrue(CheckForEqualOutput(testMatrixSize, 0, 0, initialBoard2, expectedBoard2));
+
+            string[,] initialBoard3 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "3"}
+            };
+            string[,] expectedBoard3 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "X"},
+                {"-", "-", "-", "X", "X"},
+                {"-", "-", "X", "X", "X"}
+            };
+            Assert.IsTrue(CheckForEqualOutput(testMatrixSize, 4, 4, initialBoard3, expectedBoard3));
+
+            string[,] initialBoard4 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"},
+                {"-", "3", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"}
+            };
+            string[,] expectedBoard4 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"-", "-", "-", "-", "-"},
+                {"-", "X", "-", "-", "-"},
+                {"X", "X", "X", "-", "-"},
+                {"X", "X", "X", "X", "-"},
+                {"X", "X", "X", "-", "-"}
+            };
+            Assert.IsTrue(CheckForEqualOutput(testMatrixSize, 3, 1, initialBoard4, expectedBoard4));
+
+            string[,] initialBoard5 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "3", "-"},
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"}
+            };
+            string[,] expectedBoard5 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"-", "-", "X", "X", "X"},
+                {"-", "X", "X", "X", "X"},
+                {"-", "-", "X", "X", "X"},
+                {"-", "-", "-", "X", "-"},
+                {"-", "-", "-", "-", "-"}
+            };
+            Assert.IsTrue(CheckForEqualOutput(testMatrixSize, 1, 3, initialBoard5, expectedBoard5));
+        }
+
+        [TestMethod]
+        public void HitMineTest_sizeFour()
+        {
+            const int testMatrixSize = 5;
+
+            string[,] initialBoard1 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "4", "-", "-"},
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"}
+            };
+            string[,] expectedBoard1 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"-", "X", "X", "X", "-"},
+                {"X", "X", "X", "X", "X"},
+                {"X", "X", "X", "X", "X"},
+                {"X", "X", "X", "X", "X"},
+                {"-", "X", "X", "X", "-"}
+            };
+            Assert.IsTrue(CheckForEqualOutput(testMatrixSize, 2, 2, initialBoard1, expectedBoard1));
+
+            string[,] initialBoard2 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"4", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"}
+            };
+            string[,] expectedBoard2 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"X", "X", "X", "-", "-"},
+                {"X", "X", "X", "-", "-"},
+                {"X", "X", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"}
+            };
+            Assert.IsTrue(CheckForEqualOutput(testMatrixSize, 0, 0, initialBoard2, expectedBoard2));
+
+            string[,] initialBoard3 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "4"}
+            };
+            string[,] expectedBoard3 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "X", "X"},
+                {"-", "-", "X", "X", "X"},
+                {"-", "-", "X", "X", "X"}
+            };
+            Assert.IsTrue(CheckForEqualOutput(testMatrixSize, 4, 4, initialBoard3, expectedBoard3));
+
+            string[,] initialBoard4 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"},
+                {"-", "4", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"}
+            };
+            string[,] expectedBoard4 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"-", "-", "-", "-", "-"},
+                {"X", "X", "X", "-", "-"},
+                {"X", "X", "X", "X", "-"},
+                {"X", "X", "X", "X", "-"},
+                {"X", "X", "X", "X", "-"}
+            };
+            Assert.IsTrue(CheckForEqualOutput(testMatrixSize, 3, 1, initialBoard4, expectedBoard4));
+
+            string[,] initialBoard5 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "4", "-"},
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"}
+            };
+            string[,] expectedBoard5 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"-", "X", "X", "X", "X"},
+                {"-", "X", "X", "X", "X"},
+                {"-", "X", "X", "X", "X"},
+                {"-", "-", "X", "X", "X"},
+                {"-", "-", "-", "-", "-"}
+            };
+            Assert.IsTrue(CheckForEqualOutput(testMatrixSize, 1, 3, initialBoard5, expectedBoard5));
+        }
+
+        [TestMethod]
+        public void HitMineTest_sizeFive()
+        {
+            const int testMatrixSize = 5;
+
+            string[,] initialBoard1 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "5", "-", "-"},
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"}
+            };
+            string[,] expectedBoard1 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"X", "X", "X", "X", "X"},
+                {"X", "X", "X", "X", "X"},
+                {"X", "X", "X", "X", "X"},
+                {"X", "X", "X", "X", "X"},
+                {"X", "X", "X", "X", "X"}
+            };
+            Assert.IsTrue(CheckForEqualOutput(testMatrixSize, 2, 2, initialBoard1, expectedBoard1));
+
+            string[,] initialBoard2 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"5", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"}
+            };
+            string[,] expectedBoard2 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"X", "X", "X", "-", "-"},
+                {"X", "X", "X", "-", "-"},
+                {"X", "X", "X", "-", "-"},
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"}
+            };
+            Assert.IsTrue(CheckForEqualOutput(testMatrixSize, 0, 0, initialBoard2, expectedBoard2));
+
+            string[,] initialBoard3 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "5"}
+            };
+            string[,] expectedBoard3 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "X", "X", "X"},
+                {"-", "-", "X", "X", "X"},
+                {"-", "-", "X", "X", "X"}
+            };
+            Assert.IsTrue(CheckForEqualOutput(testMatrixSize, 4, 4, initialBoard3, expectedBoard3));
+
+            string[,] initialBoard4 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"},
+                {"-", "5", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"}
+            };
+            string[,] expectedBoard4 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"-", "-", "-", "-", "-"},
+                {"X", "X", "X", "X", "-"},
+                {"X", "X", "X", "X", "-"},
+                {"X", "X", "X", "X", "-"},
+                {"X", "X", "X", "X", "-"}
+            };
+            Assert.IsTrue(CheckForEqualOutput(testMatrixSize, 3, 1, initialBoard4, expectedBoard4));
+
+            string[,] initialBoard5 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "5", "-"},
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-"}
+            };
+            string[,] expectedBoard5 = new string[testMatrixSize, testMatrixSize]
+            {
+                {"-", "X", "X", "X", "X"},
+                {"-", "X", "X", "X", "X"},
+                {"-", "X", "X", "X", "X"},
+                {"-", "X", "X", "X", "X"},
+                {"-", "-", "-", "-", "-"}
+            };
+            Assert.IsTrue(CheckForEqualOutput(testMatrixSize, 1, 3, initialBoard5, expectedBoard5));
         }
 
         private static void SetBoard(Board board, string[,] initialBoard)
@@ -58,13 +458,13 @@ namespace BattleField.Test
             }
         }
 
-        private static bool CheckIfEqual(int gameFieldSize, string[,] initialBoard, string[,] expectedBoard)
+        private static bool CheckForEqualOutput(int gameFieldSize, int x, int y, string[,] initialBoard, string[,] expectedBoard)
         {
             bool boardsAreEqual = true;
             Board board = new Board(gameFieldSize);
             SetBoard(board, initialBoard);
 
-            ExplosionHandler.HitMine(board, 1, 1);
+            ExplosionHandler.HitMine(board, x, y);
 
             for (int i = 0; i < gameFieldSize; i++)
             {
